8f2b2b1cc845c6b597af88f7e4fa591bf14bf9a4
Timothy Gurtovnik <timothygurtovnik98@gmail.com>
2025-05-08 22:47:50 +0300

    HW-fixed

--- http-text.js
+++ http-text.js
@@ -6,20 +6,27 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
 
-    if (req.url === '/user') {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const name = url.searchParams.get('name');
+
+    if (name) {
+        res.write(`<h1>Hello ${name}</h1>`);
+    }
+    else if (url.pathname === '/user') {
         res.write('<h1>Hello User</h1>');
     }
-    else if (req.url === '/group') {
+    else if (url.pathname === '/group') {
         res.write('<h1>Hello Group</h1>');
     }
     else {
-        res.write(`<h1> ${req.url}</h1>`);
+        res.write(`<h1>Вы перешли по адресу: ${url.pathname}</h1>`);
     }
 
     res.end();
 });
+
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}. Press Ctrl+C to stop.`);
